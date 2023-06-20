ccc1387b2f3569620a5a83ee8a4e89aabcc78a72
Sumit1908 <66327466+Sumit1908@users.noreply.github.com>
2023-06-20 18:21:47 +0530

    fetch php name of customer

--- admin/purchase-data.php
+++ admin/purchase-data.php
@@ -2,7 +2,14 @@
   include ('conn.php');
   include('session.php');
   if(isset($_POST['name'])){
-    $inputValue = 
+    $inputValue = $_POST['name'];
+    $query = "SELECT name FROM `tblmastercustomer` WHERE name LIKE '%$inputValue%'";
+    $result = mysqli_query($conn, $query);
+    $names = array();
+    while($row = mysqli_fetch_assoc($result)){
+      $names[] = $row['name'];
+    }
+    echo json_encode($names);
   }
   
      
